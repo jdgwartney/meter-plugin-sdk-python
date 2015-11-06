e4122d8c70e3729ce69655d1f2aa7264db423ccb
Gwartney <davidg@boundary.com>
2015-11-06 13:51:11 -0800

    Fix setup file

--- setup.py
+++ setup.py
@@ -8,10 +8,9 @@ setup(
     author_email='david_gwartney@bmc.com',
     packages=['meter_plugin_sdk', ],
     scripts=[
-        'bin/hell',
     ],
     package_data={'meter_plugin_sdk': ['templates/*']},
-    license='LICENSE.txt',
+    license='LICENSE',
     description='TrueSight Pulse Meter Plugin SDK for Python',
     long_description=open('README.txt').read(),
     install_requires=[
